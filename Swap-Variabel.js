f6f924ba611521f7522e2f8475089cd311330376
Teguh Julianto <79846289+tjulianto23@users.noreply.github.com>
2021-03-03 01:47:09 +0800

    Menambahkan cara XOR Algorithm dan ES6

--- Swap-Variabel.js
+++ Swap-Variabel.js
@@ -6,3 +6,20 @@ let a = 10;
 let b = 15;
 
 //Bagaimana cara melakukan swap kedua variabel tersebut tanpa menambahkan 1 variabel lagi?
+//Cara pertama, menggunakan XOR Swap Algorithm
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+
+console.log('a =', a);
+console.log('b =', b);
+
+//Cara kedua menggunakan ES6
+
+[a, b] = [b, a];
+
+console.log('a:', a, 'b:', b);
+
+//Refference
+//https://en.wikipedia.org/wiki/XOR_swap_algorithm
